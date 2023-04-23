7f21ba242c95795076b6f39141301678bba84644
aksweb <21it3004@rgipt.ac.in>
2023-04-23 21:28:42 +0530

    Updated code (previous giving wrong output for 7 and 13)

--- Minimum_Number_23_04_23.cpp
+++ Minimum_Number_23_04_23.cpp
@@ -1,16 +1,41 @@
-
-class Solution{
+class Solution
+{
 public:
-    int minimumNumber(int n,vector<int> &arr){
-        for(int i=0; i< n-1; i++){
-            int mini=min(arr[i], arr[i+1]);
-            int maxi=max(arr[i], arr[i+1]);
-            if(maxi%mini==0)
-                arr[i+1]=mini;
+    int minimumNumber(int n, vector<int> &arr)
+    {
+        for (int i = 0; i < n - 1; i++)
+        {
+            int mini = min(arr[i], arr[i + 1]);
+            int maxi = max(arr[i], arr[i + 1]);
+            if (mini == 0)
+            {
+                arr[i + 1] = maxi;
+                continue;
+            }
+            else if (mini == 1)
+            {
+                return 1;
+            }
+            int res = maxi % mini;
+            if (res == 0)
+                arr[i + 1] = mini;
             else
-                arr[i+1]=maxi%mini;
-        
-    }
-        return arr[n-1];
+            {
+
+                arr[i] = res;
+                arr[i + 1] = mini;
+                res = mini % arr[i];
+                if (res == 0)
+                {
+                    arr[i + 1] = arr[i];
+                }
+                else
+                {
+
+                    arr[i + 1] = res;
+                }
+            }
+        }
+        return arr[n - 1];
     }
 };
