58e1a2cf73932b98ea0144fe00c1c2bd856b2c5b
aksweb <21it3004@rgipt.ac.in>
2023-04-23 17:55:39 +0530

    New code for updated test case.

--- Minimum_Number_23_04_23.cpp
+++ Minimum_Number_23_04_23.cpp
@@ -1,12 +1,16 @@
+
 class Solution{
 public:
     int minimumNumber(int n,vector<int> &arr){
-        for(int i=0; i<n ; i++){
-            if (arr[i]%2!=0){
-                return 1;
-            } 
-        }
-        return 2;
+        for(int i=0; i< n-1; i++){
+            int mini=min(arr[i], arr[i+1]);
+            int maxi=max(arr[i], arr[i+1]);
+            if(maxi%mini==0)
+                arr[i+1]=mini;
+            else
+                arr[i+1]=maxi%mini;
         
+    }
+        return arr[n-1];
     }
 };
